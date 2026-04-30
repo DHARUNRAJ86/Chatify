a2594d5f0bc656ea1d36c59aaf16f32c3e9379fb
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-04-30 19:12:01 +0530

    Set up server with Express, dotenv, and file upload; update dependencies

--- server/app.js
+++ server/app.js
@@ -1,5 +1,30 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import {config} from 'config';
+import dotenv from "dotenv";
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
+
+const app = express();
+
+dotenv.config({ path: "./config/config.env" });
+
+app.use(cors({
+    origin:[process.env.FRONTEND_URL],
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"]
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+app.use(
+    fileUpload({
+        useTempFiles:true,
+        tempFileDir:"./temp/"
+    })
+)
+
+export default app;
+
+
