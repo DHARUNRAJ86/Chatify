3fde99819cf5b7795c59688087aeab646a40e078
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-05-25 19:25:23 +0530

    feat: set up Redux store and integrate user authentication with socket connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
+import {Loader} from 'lucide-react';
+import {useDispatch,useSelector} from 'react-redux';
+import {useEffect} from 'react';
+import {getUser,setOnlineUsers} from './store/slices/authSlice.js';
+import {connectSocket} from './lib/socket.js';
 
 const App =()=>{
-  return(
-    <div className='bg-red-500'>
-      react
-    </div>
-  )
+    const{authUser,isCheckingAuth} = useSelector((state)=>state.auth);
+    const dispatch = useDispatch();
+    
+    useEffect(()=>{
+        dispatch(getUser());
+    },[getUser])
+
+    useEffect(()=>{
+        if(authUser){
+          const socket = connectSocket(authUser._id);
+         socket.on('getOnlineUsers',(users)=>{
+              dispatch(setOnlineUsers(users));
+         })
+        }
+    },[authUser])
+
+  return <></>
 }
 export default App
