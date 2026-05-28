3a84f7b80ba831d4f4dc48a95ee14ec8f3b4d636
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-05-28 22:19:06 +0530

    feat: update Navbar component to include logo and title structure

--- client/src/components/skeletons/Navbar.jsx
+++ client/src/components/skeletons/Navbar.jsx
@@ -19,7 +19,10 @@ const Navbar =()=>{
                    {/*Left Logo */}
                    <div className='flex items-center gap-8'>
                     <Link to={"/"} className='flex items-center gap-2.5 hover:opacity-80 transition'>
-
+                      <div className='w-9 h-9 rounded-lg bg-blue-100 flex items-center justify-center'>
+                        <MessageSquare className='w-5 h-5 text-blue-500'/>
+                      </div>
+                      <h1>Talkie</h1>
                     </Link>
 
                    </div>
