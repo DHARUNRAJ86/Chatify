6bd402199ae4f95bd98b9530fa335b3fa94b7ed3
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-05-29 20:00:21 +0530

    feat: update Navbar title to correct spelling

--- client/src/components/skeletons/Navbar.jsx
+++ client/src/components/skeletons/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar =()=>{
                       <div className='w-9 h-9 rounded-lg bg-blue-100 flex items-center justify-center'>
                         <MessageSquare className='w-5 h-5 text-blue-500'/>
                       </div>
-                      <h1 className='text-lg font-bold text-gray-800'>Talkie</h1>
+                      <h1 className='text-lg font-bold text-gray-800'>Talkieee</h1>
                     </Link>
 
                    </div>
