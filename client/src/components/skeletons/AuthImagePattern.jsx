4a6ef10b10bb26aa643af06d2880f967645ef5d0
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-03 16:46:47 +0530

    feat: implement AuthImagePattern component with grid layout for loading state

--- client/src/components/skeletons/AuthImagePattern.jsx
+++ client/src/components/skeletons/AuthImagePattern.jsx
@@ -1,4 +1,19 @@
 const AuthImagePattern =({title,subtitle})=>{
-     return <></>
+     return <>
+     <div className='hidden lg:flex items-center justify-center p-12'>
+          <div className='max-w-md text-center'>
+               {/*Grid Pattern */}
+               <div className='grid grid-cols-3 gap-3 mb-8'>
+                    {
+                         [...Array(9)].map((_,index)=>{
+                              return(
+                                   <div key={index} className={`aspect-square rounded-2xl bg-gray-700/30 ${index%2===0 ? 'animate-pulse':''}`}></div>
+                              )
+                         })
+                    }
+               </div>
+          </div>
+     </div>
+     </>
 }
 export default AuthImagePattern;
