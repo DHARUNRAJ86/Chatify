106cc986e4a3111c754b7dd9c2ff87266c7696d5
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-05 15:36:25 +0530

    feat: enhance Sidebar component with user filtering and avatar display

--- client/src/components/skeletons/Sidebar.jsx
+++ client/src/components/skeletons/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar =()=>{
         dispatch(getUsers());
     }, [dispatch]);
     
-    const filteredUsers = showOnlineOnly ? users.filter(user => onlineUsers.includes(user._id)) : users;
+    const filteredUsers = showOnlineOnly ? users?.filter(user => onlineUsers.includes(user._id)) : users;
 
     if(isUsersLoading){
         return <SidebarSkeleton />;
@@ -42,7 +42,47 @@ const Sidebar =()=>{
             </span>
         </div>
       </div>
-
+      {/*Users list */}
+      <div className='overflow-y-auto w-full py-3'>
+        {
+            filteredUsers?.length > 0 && filteredUsers.map(user =>{
+                return(
+                <button key={user._id} onClick={()=>dispatch(setSelectedUser)}
+                className={`w-full p-3 flex items-center gap-3 transition-colors rounded-md ${
+                    selectedUser?._Id === user._id ? "bg-gray-200 ring-gray-200":"hover:bg-gray-200"
+                }`}
+                >
+                {/*Avatar */}
+                <div className='relative mx-auto lg:mx-0'>
+                    <img src={user?.avatar?.url || '/avatar-holder.avif'} alt={'/avatar-holder.avif'}
+                    className='w-12 h-12 object-cover rounded-full '
+                    />
+                    {
+                        onlineUsers.includes(user._id) && (
+                            <span className='absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full ring-2 ring-white'/>
+                        )
+                    }
+                </div>
+                {/*User Info */}
+                <div className='hidden lg:block text-left min-w-0'>
+                    <div className='font-medium text-gray-800 truncate'>
+                        {user.fullName}
+                    </div>
+                    <div className='text-sm text-gray-500'>
+                       {onlineUsers.includes(user._id) ? "Online" :"Offline"}
+                    </div>
+                </div>
+                </button>)
+            })}
+            {
+                filteredUsers?.length === 0 && (
+                    <div className='text-center text-gray-500 py-4'>
+                        No Online Users
+                    </div>
+                )
+            }
+        
+      </div>
     </aside>
 
 
