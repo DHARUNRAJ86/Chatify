9ab3a940446187843cb37d9369ac3157e20ce755
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-08 22:39:30 +0530

    feat: implement message fetching and auto-scrolling in ChatContainer component

--- client/src/components/skeletons/ChatContainer.jsx
+++ client/src/components/skeletons/ChatContainer.jsx
@@ -1,10 +1,53 @@
 import {useSelector,useDispatch} from 'react-redux';
-import {useRef} from 'react';
+import {useRef,useEffect} from 'react';
+import {getMessages} from '../store/slices/chatSlice.js'
+import {getSocket} from '../lib/socket.js'
+import MessageInput from './MessageInput.jsx';
+import ChatHeader from './ChatHeader.jsx';
+import MessageSkeleton from './MessageSkeleton'
+
+
 const ChatContainer = () => {
     const {messages,isMessagesLoading,selectedUser} = useSelector(state =>state.chat);
     const {authUser} = useSelector((state)=>state.auth);
     const dispatch = useDispatch();
     const messageEndRef = useRef(null);
+
+    useEffect(()=>{
+    if(!selectedUser?._id) return;
+
+    dispatch(getMessages(selectedUser._id));
+
+    const socket = getSocket();
+    if(!socket) return;
+
+},[selectedUser?._id]);
+
+    useEffect(()=>{
+        if(messageEndRef.current && messages){
+            messageEndRef.current.scrollIntoView({behavior:"smooth"})
+        }
+    },[messages])
+
+    function formatMessageTime (date){
+        return new Date(date).toLocaleTimeString("en-US",{
+            hour:"2-digit",
+            minute:"2-digit",
+            hour12:false,
+        });
+    }
+
+    
+
+    if(isMessagesLoading){
+        return (
+            <div className='flex-1 flex flex-col overflow-auto'>
+                  <ChatHeader/>
+                  <MessageSkeleton/>
+                  <MessageInput/>
+            </div>
+    )
+    }
     return <></>
 };
 
