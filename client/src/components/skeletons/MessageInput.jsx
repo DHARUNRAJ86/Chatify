edc3f74dfce00981b06a619457d7594b9967fd8c
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-13 21:48:49 +0530

    feat: add media handling logic to MessageInput component with file input and type detection

--- client/src/components/skeletons/MessageInput.jsx
+++ client/src/components/skeletons/MessageInput.jsx
@@ -1,10 +1,30 @@
 import {Image,Send,X,Video} from 'lucide-react';
 import {useState} from 'react';
+import {useRef} from 'react';
+import {useDispatch,useSelector} from 'react-redux';
+
+
+
+
 const MessageInput = () => {
   const [text,setText] = useState("");
   const[mediaPreview,setMediaPreview] = useState(null);
   const[media,setMedia] =useState(null);
   const[mediaType,setMediaType] = useState("");
+  const fileInputRef = useRef(null);
+  const dispatch = useDispatch();
+  const {selectedUser} =useSelector((state)=> state.chat);
+   
+  const handleChange =(e)=>{
+    const file =e.target.files[0];
+
+    if(!file) return;
+    setMedia(file);
+    const type = file.type;
+    if(type.startWith("image/")){
+      setMediaType("image");
+    }
+  }
   return <div>Input</div>;
 };
 
