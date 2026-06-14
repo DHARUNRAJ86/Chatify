f0267f4acdda0c6b6f90db92b9a7df539d417640
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-14 21:24:56 +0530

    feat: refactor MessageInput component to improve media handling and add message sending logic

--- client/src/components/skeletons/MessageInput.jsx
+++ client/src/components/skeletons/MessageInput.jsx
@@ -1,8 +1,7 @@
 import {Image,Send,X,Video} from 'lucide-react';
-import {useState} from 'react';
-import {useRef} from 'react';
+import {useState,useRef} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
-
+import {toast} from 'react-toastify';
 
 
 
@@ -23,8 +22,40 @@ const MessageInput = () => {
     const type = file.type;
     if(type.startWith("image/")){
       setMediaType("image");
+      const reader = new FileReader();
+      reader.onload = ()=>{
+        setMediaPreview(reader.result);
+      }
+      reader.readAsDataURL(file);
+    }else if(type.startWith("video/")){
+      setMediaType("video");
+      const videoUrl = URL.createObjectURL(file);
+      setMediaPreview(videoUrl);
+    }else{
+        toast.error("Please select an image or video file.");
+        setMedia(null);
+        setMediaPreview(null);
+        setMediaType("");
+        return;
     }
   }
+
+  const removeMedia = ()=>{
+    setMedia(null);
+    setMediaPreview(null);
+    setMediaType("");
+    if(fileInputRef.current) fileInputRef.current.value ="";
+  };
+
+  const handleSendMessage = async()=>{
+    e.preventDefault();
+    if(!text.trim() && !media) return;
+
+    const data = new FormData();
+    data.append("text",text.trim());
+    data.append("media",media);
+  }
+
   return <div>Input</div>;
 };
 
