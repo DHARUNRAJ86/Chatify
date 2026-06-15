ee720dc49357e4c8cbef377e443de73c79ca6692
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-15 22:01:28 +0530

    feat: enhance MessageInput component with socket integration for real-time message handling and fix type detection for media

--- client/src/components/skeletons/MessageInput.jsx
+++ client/src/components/skeletons/MessageInput.jsx
@@ -1,9 +1,8 @@
 import {Image,Send,X,Video} from 'lucide-react';
-import {useState,useRef} from 'react';
+import {useState,useRef,useEffect} from 'react';
 import {useDispatch,useSelector} from 'react-redux';
 import {toast} from 'react-toastify';
-
-
+import { getSocket } from '../socket';
 
 const MessageInput = () => {
   const [text,setText] = useState("");
@@ -20,14 +19,14 @@ const MessageInput = () => {
     if(!file) return;
     setMedia(file);
     const type = file.type;
-    if(type.startWith("image/")){
+    if(type.startsWith("image/")){
       setMediaType("image");
       const reader = new FileReader();
       reader.onload = ()=>{
         setMediaPreview(reader.result);
       }
       reader.readAsDataURL(file);
-    }else if(type.startWith("video/")){
+    }else if(type.startsWith("video/")){
       setMediaType("video");
       const videoUrl = URL.createObjectURL(file);
       setMediaPreview(videoUrl);
@@ -47,14 +46,36 @@ const MessageInput = () => {
     if(fileInputRef.current) fileInputRef.current.value ="";
   };
 
-  const handleSendMessage = async()=>{
+  const handleSendMessage = async(e)=>{
     e.preventDefault();
     if(!text.trim() && !media) return;
 
     const data = new FormData();
     data.append("text",text.trim());
     data.append("media",media);
-  }
+
+    // dispatch(sendMessage(data))
+    //Reset All
+
+    setText("");
+    setMedia(null);
+    setMediaPreview(null);
+    setMediaType("");
+    if(fileInputRef.current) fileInputRef.current.value ="";
+  };
+  useEffect(()=>{
+     const socket = getSocket();
+
+     if(!socket) return;
+
+     const handleNewMessage = (newMessage)=>{
+       if(newMessage.senderId === selectedUser._id || newMessage.receiverId === selectedUser._id){
+        dispatch({type:"chat/pushNewMessage",payload:newMessage});
+       }
+     };
+     socket.on("newMessage",handleNewMessage);
+     return ()=> socket.off("newMessage",handleNewMessage);
+  },[selectedUser._id])
 
   return <div>Input</div>;
 };
