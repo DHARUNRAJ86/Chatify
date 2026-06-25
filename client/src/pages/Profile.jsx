dfc75d2e9d788440e8425cad2179b29ee4e586b9
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-25 22:02:00 +0530

    feat: implement profile state management and image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,35 @@
+import {Camera,Loader2,Mail,User} from 'lucide-react'
+import {useSelector,useDispatch} from 'react-redux'
+import {useState} from 'react';
+
+
 const Profile = () => {
-  return (
-    <div>
-      <h1>Profile Page</h1>
-    </div>
-  );
+   
+   const [authUser,isUpdatingProfile] = useSelector(state => state.auth);
+
+   const [selectedImage,setSelectedImage] = useState(null);
+
+   const [formData,setFormData] =({
+       fullName:authUser?.fullName,
+       email:authUser?.email,
+       avatar:authUser?.avatar.url,
+   })
+
+   const dispatch = useDispatch();
+
+   const handleImageUpload = (e)=>{
+     const file = e.target.files[0];
+     if(!file) return;
+
+     const reader = new FileReader();
+     reader.readAsDataURL(file);
+
+     reader.onload = ()=>{
+      const base64Image = reader.result;
+      setSelectedImage(base64Image);
+     }
+   }
+   return <></>
 };
 
 export default Profile;
