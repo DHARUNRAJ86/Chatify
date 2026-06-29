4dd00548e9e0d5f3793ea86d820774a96ece3524
DHARUNRAJ86 <pdharunraj@gmail.com>
2026-06-29 21:54:35 +0530

    feat: add profile layout and structure to Profile component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -37,7 +37,19 @@ const Profile = () => {
       data.append("avatar",formData.avatar)
       dispatch(updateProfile(data));
    }
-   return <></>
+   return <>
+     <div className='min-h-screen pt-20 bg-gray-50'>
+       <div className='max-w-2xl mx-auto p-4 py-8'>
+         <div className='bg-white rounded-xl shadow-md p-6 space-y-8'>
+            <div className='text-center'>
+               <h1>Profile</h1>
+               <p>Your Profile information</p>
+            </div>
+         </div>
+       </div>
+     </div>
+   
+   </>
 };
 
 export default Profile;
